702ca9438bd85572854bf5b80215ae50363563d6
manan2110 <manan.co.in@gmail.com>
2021-09-13 22:52:00 +0530

    created models and templates changes

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+
+class DP(models.Model):
+    picture = models.ImageField(upload_to='images')
+
+
+class Profile(models.Model):
+    line1 = models.CharField(max_length=100)
+    line2 = models.CharField(max_length=100)
+    line3 = models.CharField(max_length=100)
+    line4 = models.CharField(max_length=100, blank=True)
+    email = models.CharField(max_length=256)
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=256)
+    desc = models.CharField(max_length=512)
+    github_link = models.URLField()
--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 
 def index(request):
-    return render(request, 'index.html')
+    profile = Profile.objects.get(id=1)
+    dp = DP.objects.get(id=1)
+    projects = Project.objects.all()
+    return render(request, 'index.html', {'profile': profile, 'dp': dp, 'projects': projects})
